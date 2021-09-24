23a4e6138c4c7a03e0edc091f6d8b8ea7f72b341
Juan Pablo Pinillos <jppinillos2002@gmail.com>
2021-09-24 16:46:44 -0500

    Table features created

--- DataBase/CreationTables.sql
+++ DataBase/CreationTables.sql
@@ -6,3 +6,14 @@ create table weeks(
     totalInteractions int,
     mediaInteractions int
 );
+
+create table features(
+	FEATURES_ID int primary key,
+    tree boolean default False not null,
+    sky boolean default False not null,
+    animal boolean default False not null,
+    flower boolean default False not null,
+    mountain boolean default False not null,
+    person boolean default False not null,
+    city boolean default False not null
+);
