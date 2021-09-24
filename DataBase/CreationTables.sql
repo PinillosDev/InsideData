8c595f54e01641b46a512abcd33416ee595e0604
Juan Pablo Pinillos <jppinillos2002@gmail.com>
2021-09-24 16:30:20 -0500

    Table weeks created

---
+++ DataBase/CreationTables.sql
@@ -0,0 +1,8 @@
+create table weeks(
+	WEEK_ID int primary key,
+    postPublished tinyint,
+    minutesWorked tinyint,
+    daysPublished tinyint,
+    totalInteractions int,
+    mediaInteractions int
+);
