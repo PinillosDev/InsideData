7e33517ccb435b4a33b904cff4402035a27fafc9
Juan Pablo Pinillos <jppinillos2002@gmail.com>
2021-09-28 22:43:02 -0500

    I think this is the last modify of data sets files

--- structure/sales/DataBase/SalesStorage.sql
+++ structure/sales/DataBase/SalesStorage.sql
@@ -1,10 +1,24 @@
 create table salesWeek(
 	WEEK_ID int primary key,
-    dateStartWeek date not null,
-    dateFinishWeek date not null,
     dayOfTheWeek varchar(30) not null,
     sold_units int,
     total_sale_value bigint,
     citySale varchar(100),
     countrySale varchar(100)
 );
+
+create table salesMonth(
+	MONTH_ID int primary key,
+    sold_units int,
+    total_sale_value bigint,
+    citySale varchar(100),
+    countrySale varchar(100)
+);
+
+create table salesYear(
+    YEAR_ID int primary key,
+    sold_units int,
+    total_sale_value bigint,
+    citySale varchar(100),
+    countrySale varchar(100)
+)
