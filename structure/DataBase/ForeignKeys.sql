f1f0188db04c5a24b7f3ebbfda345973ce88f9f6
Juan Pablo Pinillos <jppinillos2002@gmail.com>
2021-10-01 09:22:33 -0500

    Foreign Key employees >> clien created

--- structure/DataBase/ForeignKeys.sql
+++ structure/DataBase/ForeignKeys.sql
@@ -5,4 +5,8 @@ DATE: September 30th 2021
 
 -- clients >> payments
 alter table clients add constraint fk_clients_payments
-foreign key (fk_paymentsId) references payments(PAYMENT_ID)
+foreign key (fk_paymentsId) references payments(PAYMENT_ID);
+
+-- employees >> clients
+alter table employees add constraint fk_employees_clients
+foreign key (fk_clientId) references clients(CLIENT_ID);
